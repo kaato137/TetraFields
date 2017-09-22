5da9ade19bd2897c6b833407f285ff91c8625c40
ankuzmin <ankuzmin@odin.com>
2017-09-22 18:45:23 +0300

    Draw table content

--- grid.lua
+++ grid.lua
@@ -1,3 +1,9 @@
+local COLOR_MAP = {
+    {200, 0, 50},
+    {0, 200, 50},
+    {50, 0, 200}
+}
+
 local Grid = class("Grid", {
     x = 0, y = 0,
     cols = 0, rows = 0,
@@ -13,6 +19,8 @@ function Grid:init(x, y, cols, rows, size)
     self.rows = rows or 10
     self.size = size or 32
 
+    self.content = Grid.generateContent(self.cols, self.rows, 3)
+
     self.width = self.cols * self.size
     self.height = self.rows * self.size
 end
@@ -26,11 +34,24 @@ end
 function Grid:draw()
     local lg = love.graphics
 
+    -- Draw grid
     lg.setColor(10, 100, 200)
     drawGrid(self.x, self.y, self.cols, self.rows, self.size)
     lg.setColor(255, 255, 255)
-    drawGrid(self.x+2, self.y+2, self.cols, self.rows, self.size)
 
+    -- Draw grid content
+    for j = 1, #self.content do
+        for i = 1, #self.content[j] do
+            drawCellContent(
+                self.x + (i - 1) * self.size,
+                self.y + (j - 1) * self.size,
+                self.size, self.size,
+                self.content[j][i]
+            )
+        end
+    end
+
+    -- Draw mouse position
     local mx = love.mouse.getX()
     local my = love.mouse.getY()
     local text = mx .. ' ' .. my
@@ -60,6 +81,53 @@ function drawGrid(ox, oy, cols, rows, size)
             lg.line(x1 + ox, y1 + oy, x2 + ox, y2 + oy)
         end
     end
+end
+
+function drawCellContent(x, y, w, h, cellContent, bw, bh)
+    -- bh and bw stands for ball width and height
+    local bw = bw or 4
+    local bh = bh or 4
+    local lg = love.graphics
+
+    local rowHeight = h / #cellContent
+
+    for playeri = 1, #cellContent do
+        local ballCount = cellContent[playeri]
+        for balli = 1, ballCount do
+            local center = x + (w / 2) - (ballCount * bw)
+            lg.setColor(COLOR_MAP[playeri])
+            lg.circle(
+                "fill",
+                center + (balli-1) * bw,
+                y + (rowHeight * (playeri - 1)),
+                bw,
+                bh
+            )
+        end
+    end
+end
+
+
+function Grid.generateContent(cols, rows, numberOfPlayers)
+    local content = {}
+    for j = 1, rows do
+        content[j] = {}
+        for i = 1, cols do
+            content[j][i] = Grid.generateCellContent(numberOfPlayers)
+        end
+    end
+    return content
+end
+
+
+function Grid.generateCellContent(numberOfPlayers)
+    local cellContent = {}
+    local weights = { [1] = 10, [2] = 8, [3] = 4 }
+    for i = 1, numberOfPlayers do
+        local count = lume.weightedchoice(weights)
+        table.insert(cellContent, count)
+    end
+    return cellContent
 end
 
 return Grid
