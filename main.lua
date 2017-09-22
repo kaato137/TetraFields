7061775c9d9f093024c2665a0b53215a5e0105d0
ankuzmin <ankuzmin@odin.com>
2017-09-22 18:45:07 +0300

    Enlarge a grid

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 class = require "lib.30log"
+lume = require "lib.lume"
+
 Grid = require "grid"
 utils = require "utils"
 
@@ -6,7 +8,7 @@ function love.load()
     PLAYER_COUNT = 3
     GRID_COLS = 10
     GRID_ROWS = 10
-    GRID_SIZE = 32
+    GRID_SIZE = 48
 
     grid = Grid(100, 100, GRID_COLS, GRID_ROWS, GRID_SIZE)
 end
