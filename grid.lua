623ce9c04a91e306fb38c4cd376c0e802ab8b085
ankuzmin <ankuzmin@odin.com>
2017-10-02 19:00:59 +0300

    Refine

--- grid.lua
+++ grid.lua
@@ -97,7 +97,7 @@ function Grid:shapeCanBePlaced(x, y, shape)
     for j = 3, 0, -1 do
         for i = 3, 0, -1 do
             if (x + i > self.cols) or (y + j > self.rows) or
-               (x + i < 0) or (y + j < 0) then
+               (x + i <= 0) or (y + j <= 0) then
                 return false
             end        
         end
